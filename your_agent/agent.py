d21f9265dcb4945098f63cd1f3bb3fcdc78ef652
Julia Rodrigues <j.pelayo.rodrigues@gmail.com>
2021-09-13 23:11:23 -0300

    nao sei :(

--- your_agent/agent.py
+++ your_agent/agent.py
@@ -1,19 +1,18 @@
 import random
 import sys
 import math
-
-# from board import Board
+from queue import Queue
 
 sys.path.insert(0, '../')
 from board import *
 
 class Node(object):
-    def __init__(self, board, move, color, cost = 0):
+    def __init__(self, board, move, color, cost = None):
         self.board = board
         self.move = move
         self.color = color
         self.cost = cost
-        self.children = []
+        self.children = Queue()
     
     def get_cost(self):
         return self.cost
@@ -29,62 +28,93 @@ class Node(object):
 
     def get_children(self):
         return self.children
-
+    
+    def get_child_with_value(self, value):
+        cs = self.get_children()
+        if cs.qsize() == 0:
+            return
+        for c in range(cs.qsize()):
+            child = cs.get(c)
+            if child.get_cost() == value:
+                return child 
+        
     def get_color(self):
         return self.color
 
     def add_child(self, new_child):
-        self.children.append(new_child)
+        self.children.put(new_child)
 
 
 # Nao esqueca de renomear 'your_agent' com o nome
 # do seu agente.
 
 def disk_square_table(x,y):
-    min(x+y, 7 - x+y, x + 7 - y, 7-x + 7-y)
+    return min(x+y, 7 - x+y, x + 7 - y, 7-x + 7-y)
 
 def utility(state_node, p_color):
     acum = 0
+    if state_node.get_board().is_terminal_state():
+        num_self  = sum([1 for piece in str(state_node.get_board()) if piece==p_color])
+        num_other = sum([1 for piece in str(state_node.get_board()) if piece==state_node.get_board().opponent(p_color)])
+        if num_self > num_other:
+            return math.inf
+        if num_self < num_other:
+            return -math.inf
+        else:
+            return 0
+
     for j in range(8):
         for i in range(8):
             p = str(state_node.get_board())[8*j+i]
             if p_color == p:
                 acum += disk_square_table(i,j)
-            elif p_color == state_node.get_board().opponent(p):
+            elif state_node.get_board().opponent(p_color) == p:
                 acum -= disk_square_table(i,j)
     return acum
 
-def max_value(state_node, alpha, beta):
+def max_value(state_node, alpha, beta, depth):
     board = state_node.get_board()
-    
-    if board.is_terminal_state():
-        return utility(state_node)
+    color = state_node.get_color()
+
+    if board.is_terminal_state() or depth==0:
+        return utility(state_node, color)
 
     v = -math.inf
-
-    for s in board.legal_moves(state_node.get_color()):
-        child = Node(from_string(str(board)).process_move(s), s, board.opponent(state_node.get_color()))
- 
-        v = max(v, min_value(child, alpha, beta))
+    for s in board.legal_moves(color):
+        new_board = from_string(str(board))
+        new_board.process_move(s, color)
+        child = Node(new_board, s, board.opponent(color))
+        state_node.add_child(child)
+        v = max(v, min_value(child, alpha, beta, depth-1))
         alpha = max(alpha, v)
         if alpha >= beta:
             break
 
     state_node.set_cost(v)
+    print(id(state_node))
     return v
 
 
-def min_value(state_node, color, alpha, beta):
-    if state_node.get_board().is_terminal_state():
-        return utility(state_node)
-        
+def min_value(state_node, alpha, beta, depth):
+    board = state_node.get_board()
+    color = state_node.get_color()
+
+    if board.is_terminal_state() or depth==0:
+        return utility(state_node, color)
+
     v = math.inf
-    for s in state_node.get_board().legal_moves(color):
-        c = from_string(str(state_node.get_board())).process_move(s)
-        v = min(v, max_value(c, alpha, beta))
+
+    for s in board.legal_moves(color):
+        new_board = from_string(str(board))
+        new_board.process_move(s, color)
+        child = Node(new_board, s, board.opponent(color))
+        state_node.add_child(child)
+        v = min(v, max_value(child, alpha, beta, depth-1))
         beta = min(beta, v)
         if beta >= alpha:
             break
+
+    state_node.set_cost(v)
     return v
 
 def make_move(the_board, color):
@@ -94,11 +124,11 @@ def make_move(the_board, color):
     :param color: a character indicating the color to make the move ('B' or 'W')
     :return: (int, int) tuple with x, y indexes of the move (remember: 0 is the first row/column)
     """
-    # o codigo abaixo apenas retorna um movimento aleatorio valido para
-    # a primeira jogada com as pretas.
-    # Remova-o e coloque a sua implementacao da poda alpha-beta
-
-
-
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    root = Node(the_board, None, color)
+    print(id(root))
+    value = max_value(root, -math.inf, math.inf, 2)
+    # print(len(root.get_children()))
+    move = root.get_child_with_value(value).get_move()
+    # print(move)
+    return move
 
